7cbc02f2ed6aa3da0701c1124c0453a985c0b6ad
drzbida <55928036+drzbida@users.noreply.github.com>
2025-06-18 23:48:08 +0300

    feat(rofi): add nerd fonts mode & add workspace switcher keybind

--- .config/rofi/launcher.sh
+++ .config/rofi/launcher.sh
@@ -11,8 +11,10 @@ elif [[ $1 == "clipboard" ]]; then
     rofi -show clipboard -theme ${dir}/clip.rasi
 elif [[ $1 == "calc" ]]; then
     rofi -show calc -theme ${dir}/calc.rasi
+elif [[ $1 == "nerdy" ]]; then
+    rofi -show nerdy -theme ${dir}/nerdy.rasi
 else
-    echo "Invalid argument. Available options are: drun, clipboard, emoji, calc"
+    echo "Invalid argument. Available options are: drun, clipboard, emoji, calc, nerdy"
     exit 1
 fi
 
