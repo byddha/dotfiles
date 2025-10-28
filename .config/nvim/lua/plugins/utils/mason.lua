4eab064f75f2e22781744fff98591f7ac3ad0c0f
drzbida <55928036+drzbida@users.noreply.github.com>
2025-10-28 19:19:35 +0200

    fix(nvim): add source for roslyn mason registry

--- .config/nvim/lua/plugins/utils/mason.lua
+++ .config/nvim/lua/plugins/utils/mason.lua
@@ -45,6 +45,11 @@ return {
                 "roslyn",
                 -- "netcoredbg",
             },
+
+            registries = {
+                "github:mason-org/mason-registry",
+                "github:Crashdummyy/mason-registry",
+            }
         }
     end,
 }
