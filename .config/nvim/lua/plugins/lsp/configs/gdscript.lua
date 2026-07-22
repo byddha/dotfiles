8b96c10874a4def175d741e90f3cbbbe606487f1
drzbida <55928036+drzbida@users.noreply.github.com>
2026-07-22 19:51:09 +0300

    fix(nvim): gdscript lsp

--- .config/nvim/lua/plugins/lsp/configs/gdscript.lua
+++ .config/nvim/lua/plugins/lsp/configs/gdscript.lua
@@ -1,17 +1,4 @@
-local util = require "lspconfig.util"
-local merge = require("helpers").merge
-local is_windows = require("helpers").is_windows
-
-local config = {
-    force_setup = true,
-    single_file_support = false,
-    root_dir = util.root_pattern("project.godot", ".git"),
-    filetypes = { "gd", "gdscript", "gdscript3" },
+return {
+    filetypes = { "gdscript" },
+    root_markers = { "project.godot", ".git" },
 }
-if is_windows() then
-    config = merge(config, {
-        cmd = { "ncat", "127.0.0.1", "6005" },
-    })
-end
-
-return config
